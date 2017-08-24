058081f0fe3b838abfdb4639e9e485595f750016
Kasun <kasun.w@duosoftware.com>
2017-08-24 13:25:31 +0530

    CHeck

--- adjustment/adjustment.module.js
+++ adjustment/adjustment.module.js
@@ -31,6 +31,7 @@
 					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
 						return $q(function(resolve, reject) {
 							$timeout(function() {
+                                debugger;
                                 if($rootScope.isSuperAdmin != 'true'){
                                     msNavigationServiceProvider.saveItem('adjustment', {
                                         title    : 'adjustment',
